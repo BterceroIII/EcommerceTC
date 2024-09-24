4c9ca0eb428a926c16062002cd8001061df552d7
BterceroIII <b1tercero.cruz@gmail.com>
2024-09-23 19:42:43 -0600

    build(ecommerce.repository): create interface
    IGenericRepository and ISaleRepository

---
+++ Ecommerce.Repository/Contract/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Repository.Contract
+{
+    // esta interfaz sera un generico para todos los modelos
+    public interface IGenericRepository<TModel> where TModel : class
+    {
+        IQueryable<TModel> Consult(Expression<Func<TModel, bool>>? filtro = null);
+
+        Task<TModel> Create(TModel model);
+
+        Task<bool> Edit(TModel model);
+
+        Task<bool> Delete(TModel model);
+    }
+}
