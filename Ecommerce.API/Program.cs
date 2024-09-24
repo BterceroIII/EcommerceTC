78a3172f015dc2a0b1e10723568ad163174bafd5
BterceroIII <b1tercero.cruz@gmail.com>
2024-09-23 20:25:32 -0600

    build(ecommerce.api.programs): implent dependency injection

--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -1,6 +1,9 @@
 using Ecommerce.Repository.DBContext;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
+using Ecommerce.Repository.Contract;
+using Ecommerce.Repository.Implement;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +21,12 @@ builder.Services.AddDbContext<DbecommerceContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
 });
+
+// como es generica y no especifica el modelo que se utiliza AddTransient
+builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+// se utiliza AddScoped cuando se especifica el modelo
+builder.Services.AddScoped<ISaleRepository, SaleRepository>();
 
 
 var app = builder.Build();
