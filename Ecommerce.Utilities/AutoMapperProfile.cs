43de4c9a96beb992c2f3f11704fab1341189b17a
BterceroIII <b1tercero.cruz@gmail.com>
2024-09-24 22:14:50 -0600

    build(ecommerce.utilities): create utilities
    Create the connection of the models with DTO with automapper

---
+++ Ecommerce.Utilities/AutoMapperProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Ecommerce.DTO;
+using Ecommerce.Model;
+
+namespace Ecommerce.Utilities
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Usuario, UserDTO>();
+            CreateMap<Usuario, SessionDTO>();
+            CreateMap<UserDTO, Usuario>();
+
+            CreateMap<Categoria, CategoryDTO>();
+            CreateMap<CategoryDTO, Categoria>();
+
+            CreateMap<Producto, ProductDTO>();
+            CreateMap<ProductDTO, Producto>().ForMember(destiny => 
+            destiny.IdCategoriaNavigation, 
+            opt => opt.Ignore());
+
+            CreateMap<DetalleVenta, SaleDetailDTO>();
+            CreateMap<SaleDetailDTO, DetalleVenta>();
+
+            CreateMap<Venta, SaleDTO>();
+            CreateMap<SaleDTO, Venta>();
+
+        }
+
+    }
+}
