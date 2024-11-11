481bf413f2fb7aba6b9850356cecbe1e35851f6b
BterceroIII <b1tercero.cruz@gmail.com>
2024-11-10 22:21:00 -0600

    Create razor views

--- Ecommerce.WeAseembly/Program.cs
+++ Ecommerce.WeAseembly/Program.cs
@@ -8,6 +8,8 @@ using Blazored.Toast;
 using Ecommerce.WeAseembly.Services.Contract;
 using Ecommerce.WeAseembly.Services.Implement;
 
+using CurrieTechnologies.Razor.SweetAlert2;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -17,6 +19,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredToast();
 
+
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
@@ -24,5 +27,7 @@ builder.Services.AddScoped<IRolService, RolService>();
 builder.Services.AddScoped<IDashboardService, DashboardService>();
 builder.Services.AddScoped<ISaleService, SaleService>();
 builder.Services.AddScoped<ICartService, CartService>();
+
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
